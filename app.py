5bfa9405a39924c3da78995fc4b84184dceb7fed
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-05-05 00:55:56 -0400

    sdf

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 from flask import Flask, render_template, jsonify
 from flask_mysqldb import MySQL
 from MySQLdb.cursors import DictCursor
+from flask_socketio import SocketIO
 import os
 dbuser = os.getenv('DBUSER', 'none')
 dbpasswd = os.getenv('DBPASSWD', 'none')
 dbhost = os.getenv('DBHOST', 'none')
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['MYSQL_HOST'] = dbhost
@@ -30,5 +32,20 @@ def list_machines():
     finally:
         cursor.close()
 
+@app.route('/machine/<int:machine_id>')
+def machine_page(machine_id):
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT id, name, location, machine_status FROM machines")
+        machine = cursor.fetchall()  # Fetch all results
+        if not machine:
+            return "Machine not found", 404
+        return render_template('machine.html', machine=machine)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cursor.close()
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    socketio.run(app, debug=True, host='0.0.0.0', port=80)
