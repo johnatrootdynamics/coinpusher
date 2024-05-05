c7e803444f1235ca4b9beefb0d26b605bc1c048f
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-05-05 15:57:48 -0400

    sdf

--- client.py
+++ client.py
@@ -31,7 +31,7 @@ try:
     sio.connect('http://coinpusheronline.root-dynamics.com')
     time.sleep(2)
     update_machine_info()
-    time.sleep(20)
+    sio.wait()
     sio.disconnect()
 except socketio.exceptions.ConnectionError as e:
     print("Connection failed:", e)
