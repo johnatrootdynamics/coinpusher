09d4bcb35958ef78065d8506bde2d9c9729b1702
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-05-05 12:25:41 -0400

    sdf

--- client.py
+++ client.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
                                     on_error=on_error,
                                     on_close=on_close,
                                     on_open=on_open)
-    ws_app.run_forever()
+    ws_app.run_forever(Origin='192.168.1.1')
