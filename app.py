51616ab7d03863a4f2c48a3c2abad475762e59f9
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-05-05 01:00:51 -0400

    123sdf

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def machine_page(machine_id):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=80)
+    socketio.run(app, debug=True, host='0.0.0.0', port=80, allow_unsafe_werkzeug=True)
