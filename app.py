03c5307ff1ecde0b25c6e2a9d5297e99ecd0a201
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-05-07 00:01:01 -0400

    sdfsdf

--- app.py
+++ app.py
@@ -93,20 +93,20 @@ def handle_myevent(data):
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    cursor = mysql.connection.cursor()
-    machine_id = '1'
+    # cursor = mysql.connection.cursor()
+    # machine_id = '1'
 
-    #Update Machines Status in SQL Based on Machine ID
-    try:
-        cursor.execute("UPDATE machines SET machine_status=%s WHERE id=%s", ("2", machine_id))
-        emit('status_updated', {'machine_id': machine_id, 'new_status': '2'}, broadcast=True)
-        socketio.send("Updated machine status on disconnect")
-    except cursor.Error as e:
-        emit('error', {'message': 'Database error: ' + str(e)})
-    finally:
-        mysql.connection.commit()
-        cursor.close()
-    print("Disconnected")
+    # #Update Machines Status in SQL Based on Machine ID
+    # try:
+    #     cursor.execute("UPDATE machines SET machine_status=%s WHERE id=%s", ("2", machine_id))
+    #     emit('status_updated', {'machine_id': machine_id, 'new_status': '2'}, broadcast=True)
+    #     socketio.send("Updated machine status on disconnect")
+    # except cursor.Error as e:
+    #     emit('error', {'message': 'Database error: ' + str(e)})
+    # finally:
+    #     mysql.connection.commit()
+    #     cursor.close()
+    # print("Disconnected")
 
 @socketio.on('button_push')
 def handle_button_push(data):
