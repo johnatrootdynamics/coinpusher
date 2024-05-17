4fa5d74ce96dabd8a288e078e83e24ffc04fcb85
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-05-17 13:16:12 -0400

    sdf

--- client.py
+++ client.py
@@ -90,6 +90,51 @@ def signal_handler(sig, frame):
         sio.disconnect()
     sys.exit(0)
 
+
+def setup_gpio():
+    GPIO.setmode(GPIO.BCM)  # Use BCM GPIO numbering
+    GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set GPIO 21 as input
+    GPIO.setup(5, GPIO.OUT)  # Set GPIO 17 as output for the relay
+
+def main():
+    setup_gpio()
+    count = 0  # Initialize the counter to 0
+
+    try:
+        while True:
+            if GPIO.input(21) == GPIO.HIGH:
+                GPIO.output(5, GPIO.HIGH)  # Turn on relay
+                count += 1  # Increment count when relay is on
+                print("Relay ON, Count:", count)
+                time.sleep(.1)
+                GPIO.output(5, GPIO.LOW)
+            else:
+                if count > 0:  # Only print when there was a previous count
+                    print("Final count before relay off:", count)
+                    sio.emit('update_tickets', {'machine_id': '1', 'tickets_won': count})
+                  # Turn off relay
+                count = 0  # Reset count when relay is off
+                print("Relay OFF")
+            time.sleep(0.1)  # Sleep for 100 milliseconds
+
+    except KeyboardInterrupt:
+        print("Program terminated by user")
+    finally:
+        GPIO.cleanup()  # Ensure GPIOs are reset on exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Connect to the Flask-SocketIO server
 # try:
 #     sio.connect('http://coinpusheronline.root-dynamics.com')
@@ -106,9 +151,7 @@ try:
     sio.connect('http://coinpusheronline.root-dynamics.com')
     # Bind the signal handler to handle SIGINT (CTRL+C)
     signal.signal(signal.SIGINT, signal_handler)
-    time.sleep(2)
-    update_machine_info()
-    time.sleep(5)
+    main()
     # joinroom()
     sio.wait()
 except socketio.exceptions.ConnectionError as e:
