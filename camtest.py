346c94eb7e6d39ccf5d519aa15df960a2dea1830
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-06-28 02:21:42 -0400

    s

--- camtest.py
+++ camtest.py
@@ -21,9 +21,9 @@ def send_video():
         print("Error: Could not open video device")
         return
     cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
-    cap.set(cv2.CAP_PROP_FPS, 100)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, 30)
     while True:
         ret, frame = cap.read()
         if not ret:
