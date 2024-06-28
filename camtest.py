7c13ae30d5833d4053c7bf44f46ffbb9ca6852a6
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-06-28 02:17:24 -0400

    123

--- camtest.py
+++ camtest.py
@@ -20,7 +20,7 @@ def send_video():
     if not cap.isOpened():
         print("Error: Could not open video device")
         return
-
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
     cap.set(cv2.CAP_PROP_FPS, 100)
