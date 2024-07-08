6bca18371f72ab321237e619dd69ce87bea5e8b3
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-07-08 17:04:47 -0400

    sdf

--- camtest.py
+++ camtest.py
@@ -15,14 +15,17 @@ def disconnect():
     print("Disconnected from server")
 
 def send_video():
+
+
+    
     cap = cv2.VideoCapture('/dev/video0')  # 0 is typically the default camera
 
     if not cap.isOpened():
         print("Error: Could not open video device")
         return
     cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
     cap.set(cv2.CAP_PROP_FPS, 30)
     while True:
         ret, frame = cap.read()
