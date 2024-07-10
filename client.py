dd8ed0d8ea51a98212f144b0f666151885d7c5b5
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-07-10 19:02:34 -0400

    sdf

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socketio
+import cv2
+import base64
 import time
 import signal
 import sys
@@ -8,7 +10,36 @@ GPIO.setwarnings(False)
 
 
 
+def send_video():
 
+
+    
+    cap = cv2.VideoCapture('/dev/video0')  # 0 is typically the default camera
+
+    if not cap.isOpened():
+        print("Error: Could not open video device")
+        return
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, 30)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture image")
+            break
+
+        # Encode the frame in JPEG format
+        _, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+
+        # Send the frame to the servers
+        sio.emit('video_frame', jpg_as_text, namespace='/machine')
+
+        # To simulate a real-time streaming delay
+        sio.sleep(0.1)
+
+    cap.release()
 
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
@@ -131,6 +162,7 @@ def main():
     
     try:
         while True:
+
             if GPIO.input(21) == GPIO.HIGH:
                 GPIO.output(5, GPIO.LOW)  # Turn on relay
                 count += 1  # Increment count when relay is on
@@ -182,6 +214,7 @@ try:
         sio.connect('http://coinpusheronline.root-dynamics.com', namespaces=["/machine"])
         # Bind the signal handler to handle SIGINT (CTRL+C)
         signal.signal(signal.SIGINT, signal_handler)
+        send_video()
         main()
         sio.wait()
 except socketio.exceptions.ConnectionError as e:
